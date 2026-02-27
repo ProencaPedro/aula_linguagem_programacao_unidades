680fe164939240fec628bef6df91d88577e65254
ProencaPedro <pedro.henrikproenca@gmail.com>
2026-02-27 19:48:54 -0300

    Correção de comentarios
    Aprendi na aula presencial que 3 aspas simples, permite que crie comentários maiores

--- introdução_python.py
+++ introdução_python.py
@@ -2,10 +2,11 @@ print("Hello world!")
 Hello world!
 print(2 + 3 * 5)
 17
-
-#Variáveis são espaços alocados na memória RAM para guardar valores temporariamente.
-#A variavel especifica o tipo de dado que ela guardará.
-#Python é capaz de determinar o tipo de dado da variável com base no seu valor,ou seja, as variáveis são tipadas dinaminamente nessa linguagem.
+'''
+Variáveis são espaços alocados na memória RAM para guardar valores temporariamente.
+A variavel especifica o tipo de dado que ela guardará.
+Python é capaz de determinar o tipo de dado da variável com base no seu valor,ou seja, as variáveis são tipadas dinaminamente nessa linguagem.
+'''
 x = 10 
 nome = 'aluno'
 nota = 8.75
@@ -52,9 +53,13 @@ operacao_2 = (2 + 3) * 5
 print(f"Resultado em operacao_2 = {operacao_2}")
  Resultado em operacao_2 = 25
 
-#Tentar fazer uma equação de segundo grau y=a * x ** 2 + b * x + c, onde a,b, c são constantes.
-# O valor de y(resultado) depende do valor de x, ou seja, x é a variável independente e y a dependente.
-#Vamos solicitar para o usuário um valor de x e retornar o valor de y correspondete ao x que ele digitou.
+
+'''
+Tentar fazer uma equação de segundo grau y=a * x ** 2 + b * x + c, onde a,b, c são constantes.
+O valor de y(resultado) depende do valor de x, ou seja, x é a variável independente e y a dependente.
+Vamos solicitar para o usuário um valor de x e retornar o valor de y correspondete ao x que ele digitou.
+'''
+
 a = 2
 b = 0.5
 c = 1
@@ -77,9 +82,11 @@ print(type(x))
 <class 'float'>
 <class 'int'>
 <class 'str'>
-#O tipo de variável x é string(str), isso acontece porque ao usar a função  iput(), ela retorna uma string, independente do que o usuario digitou, sempre seá string.
-#Precisamos converter o resultado da entrada em um tipo numérico.
-#Como não sabemos se o usuário vai digitar um número inteiro ou decimal, vamos fazer a conversão usando a funcção float().
+'''
+O tipo de variável x é string(str), isso acontece porque ao usar a função  iput(), ela retorna uma string, independente do que o usuario digitou, sempre seá string.
+Precisamos converter o resultado da entrada em um tipo numérico.
+Como não sabemos se o usuário vai digitar um número inteiro ou decimal, vamos fazer a conversão usando a funcção float().
+'''
 a = 2 
 b = 0.5
 c = 1 
@@ -94,11 +101,12 @@ Digite o valor de x: 3
 O resultado de y para x = 3.0 é 20.5.
 
 
+'''
+ESTRUTURAS LÓGICAS, CONDICIONAIS E DE REPETIÇÃO EM PYTHON
 
-#ESTRUTURAS LÓGICAS, CONDICIONAIS E DE REPETIÇÃO EM PYTHON
+Em Python tudo é objeto, então uma variável também é um objeto.
 
-#Em Python tudo é objeto, então uma variável também é um objeto.
-
+'''
 #Operados relacionais
 a < b #O valor de a é menor que b?
 a <= b #O valor de a é igual OU menos que b?
@@ -109,8 +117,10 @@ a != b #O valor de a é diferente do valor de b?
 a is b #O valor de a é idêntico ao valor de b?
 a is not b #O valor de a não é idêntico ao valor de b?
 
-#Estruturas Condicionais em Python: IF, ELIF, ELSE
-#O comando if..else.. significam se..senão.. e são usados para contruir as estruturas condicionais.
+'''
+Estruturas Condicionais em Python: IF, ELIF, ELSE
+O comando if..else.. significam se..senão.. e são usados para contruir as estruturas condicionais.
+'''
 
 #Python possui uma sintaxe especial para a construção das estruturas condicionais.
 
